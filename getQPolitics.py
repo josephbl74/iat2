9fa724ad8767769cd908ab1ab63b483b50938004
josephbl74 <79768976+josephbl74@users.noreply.github.com>
2023-04-30 12:31:04 +0200

    v1.1.0 plotting rewards

--- getQPolitics.py
+++ getQPolitics.py
@@ -1,15 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Analyst():
-    def __init__(self, path):
-        self.Q=self.load(path)
+    def __init__(self):
+        # self.Q=self.load(path)
+        self.Q=np.zeros([1, 1, 1, 1])
+        self.Time=[0]
+        self.Rewards=[0]
     
-    def display(self):
+    def displayQ(self):
         print("Value Function:")
         # print(np.max(self.Q, axis=1))
         print(self.Q)
     
     def load(self, path):
-        return np.load(path)
+        lp = np.load(path)
+        self.Q=self.load(lp)
+    
+    def displayR(self):
+        print("Rewards:")
+        
+        # plot rewards table as a func. of time table
+        plt.plot(self.Time, self.Rewards)
+        plt.xlabel('Time (simulated)')
+        plt.ylabel('Reward (Time)')
+        plt.show()
+        
+    def updateTandR(self, t, r):
+        self.Time.append(t)
+        self.Rewards.append(r)
         
         
--- run_game.py
+++ run_game.py
@@ -11,20 +11,43 @@ def main():
     #controller = KeyboardController()
     # controller = RandomAgent(game.na)
     controller = QAgent(game, alpha=0.8, gamma = 0.2)
-    controller.learn(env=game, nbEpisodes=50, maxSteps=10000)
+    controller.learn(env=game, nbEpisodes=25, maxSteps=5000)
     controller.load("qtable_1_0.75_25_10000.npy")
+    
+    analyst=Analyst()
+    timeCounter=0
     
     newGame = SpaceInvaders(display=True)
     state = newGame.reset()
+
     while True:
         action = controller.select_action(state)
         state, reward, is_done = newGame.step(action)
+        timeCounter +=1
+        analyst.updateTandR(timeCounter, reward)
+        
         if(is_done):
             break
     print("Game over !")
     print("Score : ", newGame.score_val)
+    
+    #----POLITICS----
+    
+    """ 1. displaying Q """
+    
+    # ''' v1 >>> does not work no more '''
     # analyst = Analyst("qtable_1_0.75_25_10000.npy")
-    # analyst.display()
+    # analyst.displayQ()
+    
+    # ''' v2 '''
+    # analyst.load("qtable_1_0.75_25_10000.npy")
+    # analyst.displayQ()
+    
+    """ displaying R(t) """
+    
+    analyst.displayR()
+    
+    
 
 if __name__ == '__main__' :
     main()
